aa79750f45004a15091ee47809c7c96c6df59b2d
0xC0FFE2 <github1@ncloud.sbs>
2025-01-24 01:00:46 +0900

    chore : ip-api -> ipwhois 로 API게이트웨이 변경

--- src/services/IpLocationService.ts
+++ src/services/IpLocationService.ts
@@ -1,9 +1,13 @@
 export const getLocationByIP = async (ip: string): Promise<string> => {
   try {
-    const response = await fetch(`http://ip-api.com/json/${ip}`);
+    const response = await fetch(`https://ipwhois.app/json/${ip}`);
     const data = await response.json();
-    if (data.status === "success") {
-      return `${data.city}, ${data.regionName}, ${data.country}`;
+    if (data.success) {
+      const country = data.country || "Unknown Country";
+      const city = data.city || "Unknown City";
+      const region = data.region || "";
+
+      return region ? `${country}, ${city}, ${region}` : `${country}, ${city}`;
     } else {
       return "Unknown";
     }
